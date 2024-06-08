fe9e8039212545fd3c27c2db136a71a2a9e4129a
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-06-08 05:11:39 -0700

    Wrote all the edge cases for cells

--- main.go
+++ main.go
@@ -43,10 +43,82 @@ func Render(random_state [][]int) {
 	}
 }
 
+/*
+assumptions:
+ 1. All matrices will be square matrices
+*/
+func Next_Board_State(initial_board [][]int) {
+	rows := len(initial_board)
+	cols := len(initial_board[0])
+	result := [][]int{}
+	copy(result, initial_board)
+
+	/*
+		TODO:
+			2. Make sure not to confuse rows with columns. Look closely when one increases and not
+			3. Take my time to write the code
+			4. Implement tests!
+	*/
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			// create temporary array to store values for cell state computation
+			temp_arr := []int{}
+			var a int
+			var b int
+			var c int
+			var d int
+			var e int
+			var f int
+			var g int
+			var h int
+
+			// Regular top edge with 5 neighbors
+			if i == 0 && (j != 0 && j != rows-1) {
+				// top edge with 5 neighbors
+				a, b, c, d, e = initial_board[i][j-1], initial_board[i+1][j-1], initial_board[i+1][j], initial_board[i+1][j+1], initial_board[i][j+1]
+				// Regular bottom edge with 5 neighbors
+			} else if i == rows-1 && (j != 0 && j != rows-1) {
+				a, b, c, d, e = initial_board[i][j-1], initial_board[i-1][j-1], initial_board[i-1][j], initial_board[i-1][j+1], initial_board[i][j+1]
+				// Regular right edge with 5 neighbors
+			} else if j == rows-1 && (i != 0 && i != rows-1) {
+				a, b, c, d, e = initial_board[i-1][j], initial_board[i-1][j-1], initial_board[i][j-1], initial_board[i+1][j-1], initial_board[i+1][j]
+				// Regular left edge with 5 neighbors
+			} else if j == 0 && (i != 0 && i != rows-1) {
+				a, b, c, d, e = initial_board[i-1][j], initial_board[i-1][j+1], initial_board[i][j+1], initial_board[i+1][j+1], initial_board[i+1][j]
+				// Top Left Corner
+			} else if i == 0 && j == 0 {
+				a, b, c = initial_board[i][j+1], initial_board[i+1][j+1], initial_board[i+1][j]
+				// Top Right Corner
+			} else if i == 0 && j == rows-1 {
+				a, b, c = initial_board[i][j-1], initial_board[i+1][j-1], initial_board[i+1][j]
+				// Bottom Left Corner
+			} else if i == rows-1 && j == 0 {
+				a, b, c = initial_board[i-1][j], initial_board[i-1][j+1], initial_board[i][j+1]
+				// Bottom Right Corner
+			} else if i == rows-1 && j == rows-1 {
+				a, b, c = initial_board[i-1][j], initial_board[i-1][j-1], initial_board[i][j-1]
+				// Trivial case where the cell lies in the middle with 8 neighbors
+			} else {
+				a, b, c, d, e, f, g, h = initial_board[i-1][j-1], initial_board[i-1][j], initial_board[i-1][j+1], initial_board[i][j+1], initial_board[i+1][j+1], initial_board[i+1][j], initial_board[i+1][j-1], initial_board[i][j-1]
+			}
+			temp_arr = append(temp_arr, a, b, c, d, e, f, g, h)
+
+			// calculate the rules
+			count := 0
+			for _, elem := range temp_arr {
+				count += elem
+			}
+			fmt.Printf("The number of live cells is %d\n", count)
+		}
+	}
+}
+
 func main() {
-	tt := Random_State(60, 50)
-	Render(tt)
-	// for _, elem := range tt {
-	// 	fmt.Println(elem)
-	// }
+	tt := Random_State(3, 3)
+	// Render(tt)
+	for _, elem := range tt {
+		fmt.Println(elem)
+	}
+	Next_Board_State(tt)
 }
